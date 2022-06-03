d931fd850c3d6869295143c65741707341980d9d
Justin Peters <JMarvel@gmail.com>
2022-06-03 12:01:13 -0700

    change option value to numbers rather than string

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
+function add(color,animal,music,food,phone){
+  return color + animal + music + food + phone;
+}
 
 $(document).ready(function(){
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-const color = $("#color").val();
-const animal = $("#animal").val();
-const music = $("#music").val();
-const food = $("#food").val();
-const phone = $("#phone").val();
-   
-if (color === "Red" || "Blue" && animal === "Dog" && music === "Rock" && food === "Burgers" && phone === "Apple") {
-  $("#ruby").show();
-  $("#ruby").text();
-} else if (color === "Blue" && animal === "Cat" || "Bird" && music === "Pop" && food === "Tacos" || "Burgers" && phone === "Samsung") {
- 
-  $("#cSharp").show();
-  $("#cSharp").text();
-} else if (color === "Green" && animal === "Bird" && music === "Broadway" || "Rock" && food === "Sushi" && phone === "Other"){
-  
-  $("#javaScript").show(); 
-  $("#javaScript").text();
+    const color = parseInt($("#color").val());
+    const animal = parseInt($("#animal").val());
+    const music = parseInt($("#music").val());
+    const food = parseInt($("#food").val());
+    const phone = parseInt($("#phone").val());
     
-    }  
-  }) 
-});
+    const answer = add("#color"+"#animal"+"#music"+"#food"+"#phone")
+
+    $("#answer").show()
+    
+// if (color === 1 || 2 && animal === 1 && music === 1 && food === 1 && phone === 1) {
+//   $("#ruby").show();
+//   $("#ruby").text();
+// } else if (color === 2 || 3 && animal === 2 || 3 && music === 2 && food === 2 || 3 && phone === 2) {
+//   $("#cSharp").show();
+//   $("#cSharp").text();
+// } else if (color === 1 || 3 && animal === 2 || 3 && music === 1 || 3   && food === 3 && phone === 1 || 3){
+//   $("#javaScript").show(); 
+//   $("#javaScript").text();
+//     $("#lang").text(answer)
+    
+    });
+  });
