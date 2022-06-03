58883c1f9fc918889a4344cea761a02086c93aad
Justin Peters <JMarvel@gmail.com>
2022-06-03 10:37:21 -0700

    WIP trying make result reset after submission of new answers to questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-     const inputColor = $("#color").val();
-     const inputAnimal = $("#animal").val();
-     const inputMusic = $("#music").val();
-     const inputFood = $("#food").val();
-     const inputPhone = $("#phone").val();
+    const inputColor = $("#color").val();
+    const inputAnimal = $("#animal").val();
+    const inputMusic = $("#music").val();
+    const inputFood = $("#food").val();
+    const inputPhone = $("#phone").val();
  
-     if(inputColor === "Red" && inputAnimal === "Dog" && inputMusic === "Rock" && inputFood === "Burgers" && inputPhone === "Apple"){
-     $('#ruby').show();
-   } if(inputColor === "Blue" && inputAnimal === "Cat" && inputMusic === "Pop" && inputFood === "Tacos" &&inputPhone === "Samsung") 
-     $('#cSharp').show();
-     if(inputColor === "Green" && inputAnimal === "Bird" && inputMusic === "Broadway" && inputFood === "Sushi" && inputPhone === "Other"){
-      $('#javaScript').show();
-    } 
-    });
-  });
+     if (inputColor === "Red" || "Blue" && inputAnimal === "Dog" && inputMusic === "Rock" && inputFood === "Burgers" && inputPhone === "Apple") {
+    $('#ruby').text();
+    $('#ruby').show();
+      } else if (inputColor === "Blue" && inputAnimal === "Cat" || "Bird" && inputMusic === "Pop" && inputFood === "Tacos" || "Burgers" &&inputPhone === "Samsung") {
+    $('#cSharp').text();
+    $('#cSharp').show();
+      } else if
+        (inputColor === "Green" && inputAnimal === "Bird" && inputMusic === "Broadway" || "Rock" && inputFood === "Sushi" && inputPhone === "Other"){
+    $('#javaScript').text();       
+    $('#javaScript').show();
+
+        }  
+      }) 
+});
